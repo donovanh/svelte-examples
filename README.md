af916e9d65d8e315b443770fd742815ce0f50683
Donovan Hutchinson <donovanh@gmail.com>
2019-10-18 12:40:09 +0100

    Adding readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Svelte / Sapper examples
+
+A [Sapper](https://sapper.svelte.dev/) and [Svelte](https://svelte.dev/) site that shows some of the features of Svelte components.
+
+Clone, run `npm install`, then `npm run dev` to open the app at [localhost:3000](http://localhost:3000).
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,14 @@
 <style>
-  h1,
-  figure,
-  p {
+  h1 {
     text-align: center;
     margin: 0 auto;
   }
 
   h1 {
-    font-size: 2.8em;
+    font-size: 2em;
     text-transform: uppercase;
     font-weight: 700;
     margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
   }
 
   @media (min-width: 480px) {
